a236191e6063917159e1f2eb30683761b72bf740
Baibhav Choudhary <120938711+Baibhavkr001@users.noreply.github.com>
2025-04-09 18:01:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
+import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
